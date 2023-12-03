519a7f70227fcb6706104571033d474dca79dd23
hongil <sonhl0723@naver.com>
2023-12-03 23:11:37 +0900

    feature: Header 및 Information 수정
    tmp

--- src/components/resume/Career.vue
+++ src/components/resume/Career.vue
@@ -1,6 +1,26 @@
 <template>
-  <a>Career</a>
+  <div
+    :style="{
+      boxShadow: `--el-box-shadow-dark`,
+    }"
+  >
+    <el-steps direction="vertical">
+      <el-step :icon="CircleCheck">
+        test1
+      </el-step>
+      <el-step :icon="CircleCheck">
+        test2
+      </el-step>
+      <el-step :icon="CircleCheck">
+        test3
+      </el-step>
+      <el-step :icon="CircleCheck">
+        test4
+      </el-step>
+    </el-steps>
+  </div>
 </template>
-
+  
 <script setup>
+import { CircleCheck } from '@element-plus/icons-vue'
 </script>
--- src/components/resume/Information.vue
+++ src/components/resume/Information.vue
@@ -1,6 +1,22 @@
 <template>
-  <a>Information</a>
+  <div
+    :style="{
+      boxShadow: `--el-box-shadow-dark`,
+    }"
+  >
+    <el-descriptions
+      column="1"
+      title="Information"
+    >
+      <el-descriptions-item label="이름">
+        손홍일(Son Hong Il)
+      </el-descriptions-item>
+      <el-descriptions-item label="이메일">
+        hongil7626@gmail.com
+      </el-descriptions-item>
+    </el-descriptions>
+  </div>
 </template>
   
-  <script setup>
-  </script>
+<script setup>
+</script>
