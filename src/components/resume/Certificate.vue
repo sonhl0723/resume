8020b906ff9bbcc08a29395f85197c1b202cc2f2
sonhl0723 <sonhl0723@naver.com>
2023-12-09 19:55:00 +0900

    feature: Certificate 컴포넌트 수정

--- src/components/resume/Certificate.vue
+++ src/components/resume/Certificate.vue
@@ -1,5 +1,20 @@
 <template>
-  <a>Certificate</a>
+  <el-descriptions
+  title="Certificate" />
+  <el-row>
+    <el-col
+    :span="8">
+    <el-card shadow="always">
+      정보처리기사
+    </el-card>
+  </el-col>
+  <el-col
+    :span="8">
+    <el-card shadow="always">
+      oPIC
+    </el-card>
+  </el-col>
+  </el-row>
 </template>
   
   <script setup>
