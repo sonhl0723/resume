f0c4e523b0512611a882135e5a82ed2fb6c328f2
hongil.son <hongil.son@kt.com>
2024-02-06 16:56:20 +0900

    feature: 프로젝트 카드 생성

--- src/layouts/ProjectLayout.vue
+++ src/layouts/ProjectLayout.vue
@@ -1,5 +1,47 @@
 <template>
-  <a>test</a>
+  <el-row :gutter="20">
+    <el-col
+      :span="12"
+      :offset="6"
+    >
+      <el-card class="box-card">
+        <el-row
+          v-for="(value, name, index) in summary_info"
+          :key="index"
+          class="description-row"
+          :span="24"
+        >
+          <el-col
+            :span="8"
+            class="description-col-title"
+          >
+            {{ name }}
+          </el-col>
+          <el-col
+            v-if="name!='기술 스택'"
+            :span="16"
+            class="description-col-content"
+          >
+            {{ value }}
+          </el-col>
+          <el-col
+            v-else
+            class="description-col-content"
+            :span="16"
+          >
+            <el-tag
+              v-for="(stack, s_idx) in value"
+              :key="s_idx"
+              class="tag-content"
+              color="#336837"
+            >
+              {{ stack }}
+            </el-tag>
+          </el-col>
+        </el-row>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script setup>
@@ -12,7 +54,32 @@
   const summary_info = ref(null)
 
   onMounted(() => {
-    summary_info.value = project_info[router.currentRoute.value.name]
-    console.log(summary_info.value)
+    summary_info.value = (project_info[router.currentRoute.value.name])
   })
 </script>
+
+<style scoped>
+.text-item {
+  width: 100%;
+}
+.description-row {
+  margin-bottom: 8px;
+}
+
+.description-col-title {
+  text-align: left;
+  padding-left: 2rem;
+  font-weight: bold;
+  border-right: 3px solid;
+}
+
+.description-col-content {
+  text-align: left;
+  padding-left: 2rem;
+}
+
+.tag-content {
+  color: #FFFFFF;
+  font-weight: normal;
+}
+</style>
