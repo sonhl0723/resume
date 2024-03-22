242d0b4dc47b2c7c84953c7b13ca3c43d5420b14
hongil.son <hongil.son@kt.com>
2024-03-22 12:53:38 +0900

    feature: backtop 컴포넌트 추가

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -4,8 +4,17 @@
     <div class="contents-wrapper">
       <slot />
     </div>
+    <el-backtop
+      target=".contents-wrapper"
+      :right="80"
+      :bottom="100"
+      :visibility-height="-1"
+    />
   </div>
   <BaseFooter />
 </template>
     
-<script setup></script>
+<script setup>
+  import { ElBacktop } from 'element-plus';
+import BaseHeader from '../components/BaseHeader.vue';
+</script>
