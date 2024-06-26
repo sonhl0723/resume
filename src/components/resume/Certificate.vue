899b728ff1bc87378f276954f460c39ee5355ad6
hongil.son <hongil.son@kt.com>
2024-06-26 16:54:38 +0900

    feature: SAA-C03 자격증 정보 추가 및 스타일 수정

--- src/components/resume/Certificate.vue
+++ src/components/resume/Certificate.vue
@@ -15,7 +15,11 @@
             정보처리기사
           </div>
           <div class="content">
-            2023.09.01 ~
+            2023.09.01 ~ 9999.99.99
+            <el-divider />
+            23202120216W
+            <el-divider />
+            한국정보과학회
           </div>
         </el-card>
       </el-col>
@@ -31,6 +35,29 @@
           </div>
           <div class="content">
             2023.09.16 ~ 2025.09.15
+            <el-divider />
+            2A8352225312
+            <el-divider />
+            ACTFL
+          </div>
+        </el-card>
+      </el-col>
+      <el-col
+        :span="el_col"
+      >
+        <el-card
+          shadow="hover"
+          body-class="cert-card-body"
+        >
+          <div class="title">
+            AWS Certified Solutions Architect <br> Associate
+          </div>
+          <div class="content">
+            2024.06.22 ~ 2027.06.22
+            <el-divider />
+            ac9abaabf5fc41cb88fbe5a7f6a66625
+            <el-divider />
+            Amazon Web Services Training and Certification
           </div>
         </el-card>
       </el-col>
@@ -61,3 +88,22 @@
     }
   })
 </script>
+
+<style scoped>
+  .el-col {
+    margin-top: 0.5rem;
+  }
+  .title {
+    width: 100%;
+    margin-bottom: 1rem;
+  }
+  .content {
+    width: 100%;
+    display: inline-block;
+    text-align: left;
+    line-height: 2;
+  }
+  .el-card:hover {
+    box-shadow: 0 0 10px 0.5px #215015;
+  }
+</style>
--- src/components/resume/Stack.vue
+++ src/components/resume/Stack.vue
@@ -67,23 +67,29 @@
   const allActive = ref(["Language", "Database", "Middleware", "Tool"])
 
   const languages = [
-    "https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white",
-    "https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white",
-    "https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white"
+    "https://img.shields.io/badge/Python-14354C?style=flat&logo=python&logoColor=white",
+    "https://img.shields.io/badge/Java-ED8B00?style=flat&logo=openjdk&logoColor=white",
+    "https://img.shields.io/badge/C%2B%2B-00599C?style=flat&logo=c%2B%2B&logoColor=white"
   ]
   
   const databases = [
-      "https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white",
-      "https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white"
+      "https://img.shields.io/badge/MySQL-005C84?style=flat&logo=mysql&logoColor=white",
+      "https://img.shields.io/badge/PostgreSQL-316192?style=flat&logo=postgresql&logoColor=white"
   ]
 
   const middlewares = [
-    "https://img.shields.io/badge/Jenkins-D24939?style=for-the-badge&logo=Jenkins&logoColor=white",
-    "https://img.shields.io/badge/Nexus3-1B1C30?style=for-the-badge&logo=Sonatype&logoColor=white"
+    "https://img.shields.io/badge/Jenkins-D24939?style=flat&logo=Jenkins&logoColor=white",
+    "https://img.shields.io/badge/Nexus3-1B1C30?style=flat&logo=Sonatype&logoColor=white"
   ]
 
   const tools = [
-    "https://img.shields.io/badge/GIT-E44C30?style=for-the-badge&logo=git&logoColor=white",
-    "https://img.shields.io/badge/Jira-0052CC?style=for-the-badge&logo=Jira&logoColor=white"
+    "https://img.shields.io/badge/GIT-E44C30?style=flat&logo=git&logoColor=white",
+    "https://img.shields.io/badge/Jira-0052CC?style=flat&logo=Jira&logoColor=white"
   ]
   </script>
+
+<style scoped>
+  .el-image {
+    height: 24.5px;
+  }
+</style>
